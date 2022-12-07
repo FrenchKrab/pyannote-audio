21cd47f286699722986ee81b387c94f0b7a1faa9
FrenchKrab <14005967+FrenchKrab@users.noreply.github.com>
2022-12-07 15:30:47 +0100

    fix segmentation val loss permutation

--- pyannote/audio/tasks/segmentation/mixins.py
+++ pyannote/audio/tasks/segmentation/mixins.py
@@ -439,7 +439,7 @@ class SegmentationTaskMixin:
         seg_target = None
         seg_pred = None
         if self.specifications.problem == Problem.POWERSET:
-            permutated_y, _ = permutate(y_pred_powerset, y)
+            permutated_y, _ = permutate(y_pred, y)
             seg_target = self.multilabel_to_powerset(permutated_y)
             seg_pred = y_pred_powerset
         elif self.specifications.problem == Problem.MULTI_LABEL_CLASSIFICATION:
