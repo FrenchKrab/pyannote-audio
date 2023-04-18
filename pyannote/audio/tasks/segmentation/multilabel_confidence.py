ae13395c2daecd29bdc89fce1c271f7857b73d00
FrenchKrab <14005967+FrenchKrab@users.noreply.github.com>
2023-04-18 15:17:10 +0200

    set multilabel_confidence val_monitor to bce

--- pyannote/audio/tasks/segmentation/multilabel_confidence.py
+++ pyannote/audio/tasks/segmentation/multilabel_confidence.py
@@ -223,3 +223,24 @@ class MultiLabelSegmentationConfidence(MultiLabelSegmentation):
             logger=True,
         )
         return {"loss": loss}
+    
+    @property
+    def val_monitor(self):
+        """Quantity (and direction) to monitor
+
+        Useful for model checkpointing or early stopping.
+
+        Returns
+        -------
+        monitor : str
+            Name of quantity to monitor.
+        mode : {'min', 'max}
+            Minimize
+
+        See also
+        --------
+        pytorch_lightning.callbacks.ModelCheckpoint
+        pytorch_lightning.callbacks.EarlyStopping
+        """
+
+        return f"{self.logging_prefix}ValLossBCE", "min"
